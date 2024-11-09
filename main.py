b420c86ec41d31b92b0e9ce25893b60d101650b5
NewGuy250 <ndaniel220@yahoo.com>
2024-11-08 21:11:54 -0500

    added get_number_of_turtles

--- main.py
+++ main.py
@@ -1,6 +1,16 @@
+def get_number_of_turtles():
+    while True:
+        try:
+            racers = int(input("Enter the number of racers (2-10): "))
+            if 2 <= racers <= 10:
+                return racers
+            else:
+                print("Pleaser enter a number within the range 2-10.\n")
+        except ValueError:
+            print("Enter a number (2-10).\n")
 
 def main():
-    pass
+    racers = get_number_of_turtles()
 
 if __name__ == "__main__":
     main()
